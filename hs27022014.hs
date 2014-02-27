c9d17dfef3bab655050be558b0b8bd6e2e3df923
Raveline <raveline@eraveline.eu>
2014-02-27 14:02:10 +0100

    Version without any "helper function", but still somewhat ugly.

--- hs27022014.hs
+++ hs27022014.hs
@@ -1,12 +1,7 @@
 import Control.Monad
 
 divIfMultiple :: Integral a => a -> [a] -> Maybe [a]
-divIfMultiple d = mapM (dividor d) 
-
-dividor :: Integral a => a -> a -> Maybe a
-dividor d x
-    | x `mod` d == 0 = Just $ x `div` d
-    | otherwise = Nothing
+divIfMultiple d = sequence . fmap (fmap fst) . map (mfilter ((== 0) . snd) . Just . flip divMod d)
 
 main = do
 _ <- print $ divIfMultiple 3 [3, 6 .. 12]  -- Just [1,2,3,4]
