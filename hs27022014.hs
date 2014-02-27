99959453ac5df5224e413ca6325d18a4b43035cc
Raveline <raveline@eraveline.eu>
2014-02-27 11:59:46 +0100

    Removed comments - it's just an answer, and most likely not a great one.

--- hs27022014.hs
+++ hs27022014.hs
@@ -1,8 +1,5 @@
 import Control.Monad
 
--- div all the elements of the list (2nd parameter) by the first parameter
--- -- iff all the elements of the lists are exact multiple of the first parameter
--- -- returns nothing otherwise 
 divIfMultiple :: Integral a => a -> [a] -> Maybe [a]
 divIfMultiple d = mapM (dividor d) 
 
@@ -11,7 +8,6 @@ dividor d x
     | x `mod` d == 0 = Just $ x `div` d
     | otherwise = Nothing
 
---
 main = do
 _ <- print $ divIfMultiple 3 [3, 6 .. 12]  -- Just [1,2,3,4]
 _ <- print $ divIfMultiple 2 [3, 6 .. 12]  -- Nothing
