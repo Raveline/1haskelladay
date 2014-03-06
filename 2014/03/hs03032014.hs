3c28133388512535b4a3d5d3551ba20683c824e2
Raveline <raveline@eraveline.eu>
2014-03-06 10:55:26 +0100

    Thought old files were not added. Well. Here's that.

--- 2014/03/hs03032014.hs
+++
@@ -1,6 +0,0 @@
-import GHC.Exts
-
-sortByMax :: Ord a => [[a]] -> [[a]]
-sortByMax = sortWith maximum
-
-main = print $ sortByMax [[1,10],[5,5]]
--- 2014/03/hs04032014.hs
+++
@@ -1,5 +0,0 @@
-checkSort :: Ord a => [a] -> Bool
-checkSort =  undefined
-
-main = print $ checkSort ([1..10] :: [Int])
-
