12a414d09e2b04d0f6216a1933bd8948e9f5985b
Raveline <raveline@eraveline.eu>
2014-03-06 09:15:44 +0100

    Refactoring folder.

--- hs05032014.hs
+++
@@ -1,7 +0,0 @@
-import Data.Monoid
-import Data.Foldable
-chainEndos :: [a->a] -> a -> a
-chainEndos = appEndo . foldMap Endo 
-
-main = print $ chainEndos [('h':),('e':)] "llo"
-
--- hs28022014.hs
+++
@@ -1,8 +0,0 @@
-import Control.Applicative
-
-zipBinary :: [a->b->c] -> [a] -> [b] -> [c]
-zipBinary ops xs ys = getZipList $ ZipList ops <*> ZipList xs <*> ZipList ys
-
-main = print $
-  zipBinary (cycle [(+), (*)]) [1 .. 4] [2..5] -- [3, 6, 7, 20]
-
