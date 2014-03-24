6bffb42d537a5c94734bd4f88743a4e36d6f2069
Raveline <eraveline@gmail.com>
2014-03-24 13:21:03 +0100

    ^2 is more explicit.

--- 2014/03/20.hs
+++ 2014/03/20.hs
@@ -1,5 +1,5 @@
 import Data.List.Split
 
 squareList :: Int -> a -> [a] -> [[a]] 
-squareList nl def xs = chunksOf nl $ take (nl * nl) $ xs ++ repeat def
+squareList nl def xs = chunksOf nl $ take (nl^2) $ xs ++ repeat def
 
